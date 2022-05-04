eaf5ef7602f879a36b5475c4b1b2c283b09ef695
Rahul Chauhan <rchauhan9102@gmail.com>
2022-05-04 14:48:51 +0530

    Shopping Section

--- basics-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ basics-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -13,6 +13,11 @@ export class RecipeListComponent implements OnInit {
       'This is simply a test',
       'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOu3y8mMz_myEB90OA-9m7a0WoNTN1_TUvzw&usqp=CAU'
     ),
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOu3y8mMz_myEB90OA-9m7a0WoNTN1_TUvzw&usqp=CAU'
+    ),
   ];
   constructor() {}
 
