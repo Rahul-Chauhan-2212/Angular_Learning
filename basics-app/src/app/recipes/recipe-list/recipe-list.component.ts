8da8a14f7ae6d20ee8d79a28b5fbb32f37f7e9fa
Rahul Chauhan <rchauhan9102@gmail.com>
2022-05-04 11:00:39 +0530

    Recipes Content section

--- basics-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ basics-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,20 @@
+import { Recipe } from './../recipe.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOu3y8mMz_myEB90OA-9m7a0WoNTN1_TUvzw&usqp=CAU'
+    ),
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
